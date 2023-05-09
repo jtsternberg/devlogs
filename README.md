a2a106b6995aed95036ab52126ae0246dd30b42a
Justin Sternberg <justin@dsgnwrks.pro>
2023-05-09 11:46:56 -0400

    --wip-- [skip ci]

---
+++ README.md
@@ -0,0 +1,9 @@
+# A post-type Logger/debugger
+
+## Usage
+Add hooks like the following to your code to implement logging:
+```php
+```
+
+### Viewing logs
+Post-type url: `<admin_url>/edit.php?post_type=devlogs`
---
+++ devlogs.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Dev Logs
+ * Plugin URI:  https://github.com/jtsternberg/devlogs
+ * Description: A post-type Logger/debugger
+ * Author:      Jtsternberg
+ * Author URI:  https:/jtsternberg.com
+ * Version:     1.0.0
+ * Text Domain: devlogs
+ * Domain Path: languages
+ *
+ * WPForms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * WPForms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WPForms. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Plugin version.
